5e5f79a04e6a4362396241ab52bc9340431fcb3e
CBT2019 <42825375+CBT2019@users.noreply.github.com>
2019-06-27 16:33:18 -0400

    Update PayMerkleExtension.sol

--- contracts/PayMerkleExtension.sol
+++ contracts/PayMerkleExtension.sol
@@ -1,45 +1,42 @@
 pragma solidity ^0.4.18;
 
-contract PayMerkleExtended {
+contract PayMerkle {
     address public channelSender;
     address public channelRecipient;
     uint public startDate;
     uint public channelTimeout;
     bytes32 public root;
 
-    function PayMerkleExtended(address to, uint _timeout, bytes32 _root) public payable {
-        require(msg.value>0);
+    function PayMerkle(address to, uint _timeout, bytes32 _root) payable public {
         channelRecipient = to;
         channelSender = msg.sender;
         startDate = now;
         channelTimeout = _timeout;
         root = _root;
     }
-    function AddBalance(bytes32 _newRoot) public payable {
-      if (root < _newRoot)
-          root = keccak256(root, _newRoot);
-      else
-          root = keccak256(_newRoot, root);
-    }
   function CloseChannel(uint256 _amount, uint256 _random, bytes32[] proof) public {
+        require(msg.sender==channelRecipient);
         bytes32 computedHash = keccak256(_amount,_random);
-        require(verifyMerkle(root, computedHash, proof));
+        for (uint256 i = 0; i < proof.length; i++) {
+          bytes32 proofElement = proof[i];
+          if (computedHash < proofElement)
+            computedHash = keccak256(computedHash, proofElement);
+          else
+            computedHash = keccak256(proofElement, computedHash);
+          }
+        require(computedHash==root);
         channelRecipient.transfer(_amount);
         selfdestruct(channelSender);
-    }
-    
-    function verifyMerkle (bytes32 _root, bytes32 leaf, bytes32[] proof) public pure returns (bool) {
-      bytes32 computedHash = leaf;
-      for (uint256 i = 0; i < proof.length; i++) {
-          if (computedHash < proof[i])
-            computedHash = keccak256(computedHash, proof[i]);
-          else
-            computedHash = keccak256(proof[i], computedHash);
-          }
-        return computedHash==_root;
     }
     function ChannelTimeout() public {
         require(now >= startDate + channelTimeout);
         selfdestruct(channelSender);
     }
+    
+    function AddBalance(bytes32 _newRoot) public payable {
+      if (root < _newRoot)
+          root = keccak256(root, _newRoot);
+      else
+          root = keccak256(_newRoot, root);
+    }    
 }
